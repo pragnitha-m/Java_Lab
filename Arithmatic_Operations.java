3996fd108045e8b7f3f21612046b0071648fce72
Pragnitha Midamaneni <120701618+pragnitha-m@users.noreply.github.com>
2023-06-06 10:49:28 +0530

    Create Arithmatic_Operations.java

---
+++ Arithmatic_Operations.java
@@ -0,0 +1,18 @@
+import java.io.*;
+class a_operations
+{
+	public static void main(String args[])
+	{
+		int a=15,b=3,add=0,sub=0,mul=0,div=0;
+		System.out.println("Num1 = "+a);
+		System.out.println("Num2 = "+b);
+		add = a+b;
+		sub = a-b;
+		mul = a*b;
+		div = a/b;
+		System.out.println("Addition of Num1 and Num2 = "+add);
+		System.out.println("Addition of Num1 and Num2 = "+sub); 
+		System.out.println("Addition of Num1 and Num2 = "+mul); 
+		System.out.println("Addition of Num1 and Num2 = "+div); 
+	}
+}
